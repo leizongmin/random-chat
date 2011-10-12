c44d6c0df52c69133aee404a7878a8459734f934
Lei Zongmin <leizongmin@gmail.com>
2011-10-12 12:07:00 +0800

    使用v0.1.11版的QuickWeb

--- start.js
+++ start.js
@@ -8,34 +8,36 @@
 var web = require('QuickWeb');
 var io = require('socket.io');
 var mustache = require('mustache');
-//var Room = require('./lib/room.js');
-var RandomRoom = require('./lib/random');
+ //--------------------------------------------------------------------------------
  
  
 /* 配置QuickWeb */
-var web = require('QuickWeb');
-web.set('home_path', './html');				// 网站根目录
-web.set('code_path', './code');				// 处理程序目录
-web.set('template_path', './html');			// 模板目录
-web.set('tmp_path',	'./tmp');				// 临时目录
-web.set('session_cookie_maxage', 3600 * 24 * 7);	// Session的Cookie存储时间
-// 模板引擎
-web.set('render_to_html', function (str, view) {
-	console.log(view);
-	return mustache.to_html(str, view);
+web.set({
+	'template_path':			'./html',				// 模板目录
+	'session_cookie_maxage':	3600 * 24 * 7,			// Session的Cookie存储时间
+	'render_to_html':			function (str, view) {		// 模板引擎
+		return mustache.to_html(str, view);
+	}
 });
-// Session引擎
-require('./lib/session')(web);
+
+// 基于MongoDB的Session引擎
+require('./lib/session');
+
 // 监听80端口
 var s = web.create(80);
+//--------------------------------------------------------------------------------
 
 
 /* 配置socket.io */
 io = io.listen(s);
-io.set('transports', ['htmlfile', 'xhr-polling', 'jsonp-polling']);
+// 设置客户端连接方式，由于NAE中不支持websocket连接，此处只允许xhr-polling, jsonp-polling, htmlfile
+io.set('transports', ['xhr-polling', 'jsonp-polling', 'htmlfile']);
 // 注册房间
+var RandomRoom = require('./lib/random');
 var room = new RandomRoom('random', io);
 global.room = room;
+//--------------------------------------------------------------------------------
+
 
 /* 日志输出 */
 web.setLogLevel(3);
